a4340261816704f087f571a2823c8b4991819550
vibha987789 <vibhaa.87073@gmail.com>
2025-10-08 20:27:36 +0530

    pro48.php

---
+++ pro48.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+  <title>User Registration</title>
+</head>
+<body>
+  <h2>User Registration Form</h2>
+  <form method="post" action="register.php">
+    Username: <input type="text" name="username"><br><br>
+    Email: <input type="email" name="email"><br><br>
+    Password: <input type="password" name="password"><br><br>
+    <input type="submit" value="Register">
+  </form>
+</body>
+</html>
